d285bbb4fc16f24944bb8352037a0b6cf0ba0595
Kamil Kowalczyk <kkowalczyk@ifxpayments.com>
2021-05-04 17:24:55 +0200

    Test

--- console.php
+++ console.php
@@ -4,11 +4,16 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Command\MessageCommand;
+use App\HttpClient\SymfonyHttp;
+use App\Command\AskCoinCommand;
 use Symfony\Component\Console\Application;
+
 
 $app = new Application();
 
 $app->add(new MessageCommand());
+$app->add(new AskCoinCommand());
 
 $app->run();
+
 
--- src/Command/MessageCommand.php
+++ src/Command/MessageCommand.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
@@ -8,51 +7,29 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
+
 class MessageCommand extends Command
 {
 
-    protected function greetUser(InputInterface $input, OutputInterface $output)
-    {
-        // outputs multiple lines to the console (adding "\n" at the end of each line)
-        $output -> writeln([
-            '========== Future Billionaire Greetings Console App ==========',
-            '==============================================================',
-            '',
-        ]);
-
-        // outputs a message without adding a "\n" at the end of the line
-        $output -> write($this -> getGreeting() .', '. $input -> getArgument('msg'));
-    }
-
-
-    protected function configure()
+    protected function configure(): void
     {
         $this->setName('msg')
-            ->setDescription('The command asks for the first currency')
-            ->setHelp('This command allows you to enter the first currency of the currency pair')
-            ->addArgument('msg',  InputArgument::REQUIRED, 'Enter the first currency: ');
+            ->setDescription('The command asks for the first and second currency')
+            ->setHelp('This command allows you to enter the currency pair')
+            ->addArgument('firstcoin',  InputArgument::REQUIRED, 'Enter the first currency: ')
+            ->addArgument('secondcoin', InputArgument::REQUIRED, 'Enter the second currency: ');
     }
 
-    protected function getGreeting(): string
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $time = date("H");
-        if ($time < "12") {
-            return "Good morning";
-        } else
-            if ($time >= "12" && $time < "17") {
-                return "Good afternoon";
-            } else
-                if ($time >= "17" && $time < "19") {
-                    return "Good evening";
-                } else
-                    if ($time >= "19") {
-                        return "Good night";
-                    }
-    }
+        $fcoin = 'Hi ' . $input->getArgument('firstcoin');
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $this -> greetUser($input, $output);
+        $scoin = $input->getArgument('secondcoin');
+        if ($scoin) {
+            $fcoin .= ' ' . $scoin;
+        }
+
+        $output->writeln($fcoin.'!');
+
         return Command::SUCCESS;
-
     }}
